daa110abf8e9bead985a2095dd2f58a139aee4db
Santosh Kumar <sntshkmr60@gmail.com>
2022-10-09 16:17:07 +0530

    Add mongo connectivity

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require("express")
+const mongoose = require("mongoose")
 
 const serverConfig = require("./configs/server.config")
+const dbConfig = require("./configs/db.config")
+
+mongoose.connect(dbConfig.DB_URL)
+const db = mongoose.connection
+
+db.on("error", () => {
+  console.log("Error while connection to the MongoDB server");
+})
+
+db.on("open", () => {
+  console.log("Connected to the MongoDB server");
+})
 
 const app = express()
 app.use(express.json())
---
+++ configs/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DB_URL: "mongodb://127.0.0.1/find_my_restaurant"
+}
